f394446918dd0a0bace6b678b99c029c6f8b59ac
lovelyladylove <lovewagemann@gmail.com>
2019-04-17 12:49:12 +0200

    Updates

---
+++ precache-manifest.016b5d41540085b01189bf5d7e94569d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploytest/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "49395a0131567ac317f1",
+    "url": "/deploytest/static/js/runtime~main.409920f8.js"
+  },
+  {
+    "revision": "22a99596e6103d9627fc",
+    "url": "/deploytest/static/js/main.c289f27c.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/deploytest/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "22a99596e6103d9627fc",
+    "url": "/deploytest/static/css/main.7423cba0.chunk.css"
+  },
+  {
+    "revision": "0791760ac819c536f02a24e97eb7acdb",
+    "url": "/deploytest/index.html"
+  }
+];
